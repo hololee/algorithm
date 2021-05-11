0d74ad7cf169d5540e89b65cb85f25af0920f192
hololee <lccandol@naver.cm>
2021-05-11 20:13:27 +0900

    Update to 21/05/11

--- baekjoon/210511/5639.py
+++ baekjoon/210511/5639.py
@@ -1,4 +1,14 @@
 import sys
 
+
+def post_order(node):
+
+
+class Node:
+    def __init__(self, m, l=None, r=None):
+        self.m = m
+        self.l = l
+        self.r = r
+
+
 if __name__ == '__main__':
-   print()
